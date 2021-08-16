5863e38d6a394794386e8321aab7fed430770c15
Damian Joseph <damianvjoseph@gmail.com>
2021-08-16 21:21:39 +1000

    add new data

--- js/index.js
+++ js/index.js
@@ -10,8 +10,8 @@ const urls = [
   "https://www.quandl.com/api/v3/datasets/ISM/MAN_EMPL?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
   "https://www.quandl.com/api/v3/datasets/ISM/MAN_PROD?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
   "https://www.quandl.com/api/v3/datasets/ISM/MAN_DELIV?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
-  "https://www.quandl.com/api/v3/datasets/ISM/MAN_INVENT?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
-  "https://www.quandl.com/api/v3/datasets/ISM/MAN_CUSTINV?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
+  //"https://www.quandl.com/api/v3/datasets/ISM/MAN_INVENT?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
+  //"https://www.quandl.com/api/v3/datasets/ISM/MAN_CUSTINV?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
 
 ]
 
@@ -26,8 +26,8 @@ const fetchData = async () => {
     var man_empl = response[2].dataset.data;
     var man_prod = response[3].dataset.data;
     var man_deliv = response[4].dataset.data;
-    var man_invent = response[5].dataset.data;
-    var man_custinv = response[6].dataset.data;
+    //var man_invent = response[5].dataset.data;
+    //var man_custinv = response[6].dataset.data;
 
     let PMI = [], areaPMI = [], newOrder = [], manEmpl = [], manProd = [], manDeliv = [], manInvent = [], manCustInv = [];
 
@@ -37,8 +37,8 @@ const fetchData = async () => {
       manEmpl.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_empl[idx][1]});
       manProd.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_prod[idx][1]});
       manDeliv.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_deliv[idx][1]});
-      manInvent.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_invent[idx][1]});
-      manCustInv.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_custinv[idx][1]});
+     // manInvent.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_invent[idx][1]});
+     // manCustInv.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_custinv[idx][1]});
       areaPMI.push({x: Date.parse(ism_pmi_data[idx][0]), y: ism_pmi_data[idx][1]-50});
 
     });
@@ -51,8 +51,8 @@ const fetchData = async () => {
       {name: 'Manufacturing Employment', points: manEmpl,yAxis: 'leftAxis',color:colors[21]},
       {name: 'Manufacturing Production', points: manProd,yAxis: 'leftAxis'},
       {name: 'Manufacturing Deliveries', points: manDeliv,yAxis: 'leftAxis'},
-      {name: 'Manufacturing Inventories', points: manInvent,yAxis: 'leftAxis'},
-      {name: 'Manufacturing Customers Inventories', points: manCustInv,yAxis: 'leftAxis'},
+     // {name: 'Manufacturing Inventories', points: manInvent,yAxis: 'leftAxis'},
+     // {name: 'Manufacturing Customers Inventories', points: manCustInv,yAxis: 'leftAxis'},
       {name: 'PMI Area', points: areaPMI,type:'area',yAxis: 'rightAxis',color:colors[15]},
     ];
 
