b917fba63d72e602e7d21da9301735f1250e2dbd
Damian Joseph <damianvjoseph@gmail.com>
2021-08-16 21:14:03 +1000

    add new data

--- js/index.js
+++ js/index.js
@@ -4,67 +4,111 @@
 var colors = JSC.getPalette(0);
 
 
-// const urls = [
-//   "https://www.quandl.com/api/v3/datasets/ISM/MAN_PMI?start_date=1948-01-01&end_date=2021-07-01&api_key=hQqbsfakqXiqavyb4SV9",
-//   "https://www.quandl.com/api/v3/datasets/ISM/MAN_NEWORDERS?start_date=1948-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
-// ]
+const urls = [
+  "https://www.quandl.com/api/v3/datasets/ISM/MAN_PMI?start_date=1989-01-01&end_date=2021-07-01&api_key=hQqbsfakqXiqavyb4SV9",
+  "https://www.quandl.com/api/v3/datasets/ISM/MAN_NEWORDERS?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
+  "https://www.quandl.com/api/v3/datasets/ISM/MAN_EMPL?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
+  "https://www.quandl.com/api/v3/datasets/ISM/MAN_PROD?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
+  "https://www.quandl.com/api/v3/datasets/ISM/MAN_DELIV?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
+  "https://www.quandl.com/api/v3/datasets/ISM/MAN_INVENT?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
+  "https://www.quandl.com/api/v3/datasets/ISM/MAN_CUSTINV?start_date=1989-01-01&end_date=2021-07-01&column_index=5&api_key=hQqbsfakqXiqavyb4SV9",
 
-// const fetchData = async () => {
-//   try {
-//     const response = await Promise.all(
-//       urls.map(url => fetch(url).then(res => res.json()))
-//     )
+]
+
+const fetchData = async () => {
+  try {
+    const response = await Promise.all(
+      urls.map(url => fetch(url).then(res => res.json()))
+    )
     
-//     var series = response[0].dataset.data
-//     console.log(series)
-//     var series = response[1].dataset.data
-//     console.log(series)
+    var ism_pmi_data = response[0].dataset.data;
+    var man_new_orders = response[1].dataset.data;
+    var man_empl = response[2].dataset.data;
+    var man_prod = response[3].dataset.data;
+    var man_deliv = response[4].dataset.data;
+    var man_invent = response[5].dataset.data;
+    var man_custinv = response[6].dataset.data;
+
+    let PMI = [], areaPMI = [], newOrder = [], manEmpl = [], manProd = [], manDeliv = [], manInvent = [], manCustInv = [];
+
+    ism_pmi_data.forEach((val, idx) => {
+      PMI.push({x: Date.parse(ism_pmi_data[idx][0]), y: ism_pmi_data[idx][1]});
+      newOrder.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_new_orders[idx][1]});
+      manEmpl.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_empl[idx][1]});
+      manProd.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_prod[idx][1]});
+      manDeliv.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_deliv[idx][1]});
+      manInvent.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_invent[idx][1]});
+      manCustInv.push({x: Date.parse(ism_pmi_data[idx][0]), y: man_custinv[idx][1]});
+      areaPMI.push({x: Date.parse(ism_pmi_data[idx][0]), y: ism_pmi_data[idx][1]-50});
+
+    });
+
+
+
+    var data_series = [
+      {name: 'Manufacturing PMI', points: PMI,type:'line',yAxis: 'leftAxis'},
+      {name: 'Manufacturing New Orders', points: newOrder,yAxis: 'leftAxis'},
+      {name: 'Manufacturing Employment', points: manEmpl,yAxis: 'leftAxis',color:colors[21]},
+      {name: 'Manufacturing Production', points: manProd,yAxis: 'leftAxis'},
+      {name: 'Manufacturing Deliveries', points: manDeliv,yAxis: 'leftAxis'},
+      {name: 'Manufacturing Inventories', points: manInvent,yAxis: 'leftAxis'},
+      {name: 'Manufacturing Customers Inventories', points: manCustInv,yAxis: 'leftAxis'},
+      {name: 'PMI Area', points: areaPMI,type:'area',yAxis: 'rightAxis',color:colors[15]},
+    ];
+
+    renderChart2(data_series);
+
     
-//   } catch (error) {
-//     console.log("Error", error)
-//   }
+  } catch (error) {
+    console.log("Error", error)
+  }
+}
+fetchData()
+
+
+
+
+
+
+
+// function fetch_ISM_MAN_PMI(callback) {
+//     fetch('https://www.quandl.com/api/v3/datasets/ISM/MAN_PMI?start_date=1948-01-01&end_date=2021-07-01&api_key=hQqbsfakqXiqavyb4SV9')
+//        .then(response => response.json())
+//        .then(json => callback(null, json.dataset))
+//        .catch(error => callback(error, null))
 // }
-// fetchData()
 
-
-function fetch_ISM_MAN_PMI(callback) {
-    fetch('https://www.quandl.com/api/v3/datasets/ISM/MAN_PMI?start_date=2000-01-01&end_date=2021-07-01&api_key=hQqbsfakqXiqavyb4SV9')
-       .then(response => response.json())
-       .then(json => callback(null, json.dataset))
-       .catch(error => callback(error, null))
-}
-
-fetch_ISM_MAN_PMI((error, ISM_MAN_PMI) => {
-    if (error) 
-        console.log(error)
-    else 
-       // console.log(ISM_MAN_EMPL.data)
-      var series = csvToSeries2(ISM_MAN_PMI.data)
-      renderChart2(series);
-});
+// fetch_ISM_MAN_PMI((error, ISM_MAN_PMI) => {
+//     if (error) 
+//         console.log(error)
+//     else 
+//        // console.log(ISM_MAN_EMPL.data)
+//       var series = csvToSeries2(ISM_MAN_PMI.data)
+//       renderChart2(series);
+// });
 
 
 
-function csvToSeries2(text) {
-  const ism_y = 'PMI';
-  const date_x = 'Date';
-  const new_order_y = 'New_Orders';
-  let dataAsJson = text;
-  let PMI = [], areaPMI = [], newOrder =[];
+// function csvToSeries2(text) {
+//   const ism_y = 'PMI';
+//   const date_x = 'Date';
+//   const new_order_y = 'New_Orders';
+//   let dataAsJson = text;
+//   let PMI = [], areaPMI = [], newOrder =[];
   
-  dataAsJson.forEach(function (row) {
-    //add either to Black, White arrays, or discard.
-    PMI.push({x: Date.parse(row[0]), y: row[1]});
-    areaPMI.push({x: Date.parse(row[0]), y: row[1]-50});
-    newOrder.push({x: Date.parse(row[0]), y: row[1]})
+//   dataAsJson.forEach(function (row) {
+//     //add either to Black, White arrays, or discard.
+//     PMI.push({x: Date.parse(row[0]), y: row[1]});
+//     areaPMI.push({x: Date.parse(row[0]), y: row[1]-50});
+//     newOrder.push({x: Date.parse(row[0]), y: row[1]})
 
-  });
-  return [
-    {name: 'PMI', points: PMI,type:'line',yAxis: 'leftAxis'},
-    {name: 'newOrder', points: PMI,yAxis: 'leftAxis'},
-    {name: 'areaPMI', points: areaPMI,type:'area',yAxis: 'rightAxis',color:colors[15]},
-  ];
-}
+//   });
+//   return [
+//     {name: 'PMI', points: PMI,type:'line',yAxis: 'leftAxis'},
+//     {name: 'newOrder', points: PMI,yAxis: 'leftAxis'},
+//     {name: 'areaPMI', points: areaPMI,type:'area',yAxis: 'rightAxis',color:colors[15]},
+//   ];
+// }
 
 
 function renderChart2(series) {
@@ -75,14 +119,31 @@ function renderChart2(series) {
         id: 'leftAxis',
         defaultTick_label_color: colors[15],
         label_text: 'PMI',
+        defaultTick_enabled: true,
+
+        scale: {
+          /*Define an axis range of 0-100*/
+          //range: [30, 70]
+          range: { min: 20, max: 90, padding: 0.1 }
+        }
+
+
       },
       {
         id: 'rightAxis',
         defaultTick_label_color: colors[15],
-
+        defaultTick_enabled: true,
         orientation: 'right',
+
+        scale: {
+          /*Define an axis range of 0-100*/
+          //range: [-20, 20]
+          range: { min: -30, max: 40, padding: 0.1 }
+        }
+
       },
     ],
+
     chartArea: { 
         fill: colors[19]
       },
@@ -92,18 +153,7 @@ function renderChart2(series) {
       crosshair_enabled: true, 
       scale: { type: "time" } 
     },
-    defaultTooltip: { 
-    combined: true, 
-    /* For a list of possible tokens, visit Point List Tokens section here: 
-     * https://jscharting.com/tutorials/js-chart-labels/token-reference/ 
-     */
-    label_text: '%xValue<hr>%points'
-    }, 
-    defaultSeries: { 
-    defaultPoint_tooltip: 
-      '%icon <span style="width:70px">%seriesName</span> %yValue', 
-    opacity: 0.7 
-    }, 
+
     axisToZoom: 'x', 
       /*X Axis Time Zoom limit*/
       xAxis: { 
