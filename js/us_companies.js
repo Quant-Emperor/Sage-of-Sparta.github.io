9ec6953ac4dc7f71300871e27dc271bd9cf3712c
Damian Joseph <damianvjoseph@gmail.com>
2021-08-22 08:34:54 +1000

    working on table display

--- js/us_companies.js
+++ js/us_companies.js
@@ -4,42 +4,13 @@
 
 // JS 
   
-JSC.fetch('https://raw.githubusercontent.com/Sage-of-Sparta/Sage-of-Sparta.github.io/master/data/usa_stocks/us_sector_Auto-Tires-Trucks.csv').then( 
-  function(response) { 
-    if (response.ok) { 
-      response.text().then(function(text) { 
-        var parsedData = JSC.csv2Json(text); 
-        parsedData.headerCol = true; 
-        parsedData.columns = [ 
-          { value: 'Company Name' }, 
-          { value: 'Ticker'}, 
-          { value: 'Exchange' }, 
-          { value: 'Sector' }, 
-          { value: 'Industry' }, 
-          { value: 'Market Cap (mil)' },
-          { value: 'Month of Fiscal Yr End' },
-          { value: 'P/E (Trailing 12 Months)' },
-          { value: 'P/E (F1)' },
-          { value: 'P/E (F2)' },
-          { value: 'F0 Consensus Est' },
-          { value: 'F1 Consensus Est' },
-          { value: 'F2 Consensus Est' },
-          { value: 'EPS Growth F1' },
-          { value: 'EPS Growth F2' },
-          { value: 'F1 PEG' },
-          { value: 'F2 PEG' },
+var foo;
+
+
+foo = fetch("https://raw.githubusercontent.com/Sage-of-Sparta/Sage-of-Sparta.github.io/master/data/usa_stocks/us_sector_Auto-Tires-Trucks.csv")
+	  .then(response => response.text()); 
+	    
 
 
 
-        ]; 
-        JSC.Grid( 
-          'gridDiv', 
-          parsedData 
-        ).then(function(grid) {}); 
-      }); 
-    } else { 
-      console.error('Problem loading csv data.'); 
-    } 
-  } 
-); 
-
+console.log(foo);
