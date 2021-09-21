464144b1770e2673f662c3a7d6a0f80a7f61b52a
Damian Joseph <damianvjoseph@gmail.com>
2021-09-21 12:25:47 +1000

    add secrets of black candle

--- js/blackcandle.js
+++ js/blackcandle.js
@@ -30,18 +30,43 @@ async function fetchData(urlstrings) {
 
     nyse_json.forEach((val, idx) => { 
       if ((nyse_json[idx]["price"] < nyse_json[idx]["open"]) && (nyse_json[idx]["close"] >= nyse_json[idx]["previousClose"] ) ) {
-        data.push({symbol: nyse_json[idx]["symbol"]});
+        data.push({symbol: nyse_json[idx]["symbol"], name: nyse_json[idx]["name"], price: nyse_json[idx]["price"], change: nyse_json[idx]["change"], changesPercentage: nyse_json[idx]["changesPercentage"]});
       }
 
     });
     nasdaq_json.forEach((val, idx) => {
       if ((nasdaq_json[idx]["price"] < nasdaq_json[idx]["open"]) && (nasdaq_json[idx]["close"] >= nasdaq_json[idx]["previousClose"] ) ) {
-        data.push({symbol: nasdaq_json[idx]["symbol"]});
+        data.push({symbol: nasdaq_json[idx]["symbol"], name: nasdaq_json[idx]["name"], price: nasdaq_json[idx]["price"], change: nasdaq_json[idx]["change"], changesPercentage: nasdaq_json[idx]["changesPercentage"]});
       }
     });
     //console.log(nyse)
 
-    datagrid(data);
+    //datagrid(data);
+    //loaddatatable(data);
+      $(document).ready(function() {
+      
+      //console.log(data);
+      var column_list = [
+        { title: "symbol"},
+        { title: "name"},
+        { title: "price"},
+        { title: "change"},
+        { title: "changesPercentage"},
+
+      ];
+
+      var data_val = data.map(el => Object.values(el));
+
+          var dataTables = $('#table').DataTable ({
+              retrieve: true,
+              paging: false,
+              data:data_val,
+              columns : column_list
+          });
+          //dataTables.rows.add(data).draw();
+  });
+
+
 
   } catch (error) {
     console.log("Error", error)
@@ -60,4 +85,21 @@ function datagrid(data) {
       }
     ] 
   }); 
+}
+
+
+function loaddatatable(data) {
+ 
+    //var aDemoItems  = oResults.lDemographicItems; //
+    //var jsonString = JSON.stringify(aDemoItems  ) //for testing
+     
+   //Load  datatable
+    var oTblReport = $("#table")
+ 
+    oTblReport.DataTable ({
+        "data" : data,
+        "columns" : [
+            { "data" : "symbol" },
+        ]
+    });
 }
