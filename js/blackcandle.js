087d1e69f93dd3b9e6d6ea163a44b032a11fff87
Damian Joseph <damianvjoseph@gmail.com>
2021-09-22 08:01:10 +1000

    add data

--- js/blackcandle.js
+++ js/blackcandle.js
@@ -23,21 +23,36 @@ async function fetchData(urlstrings) {
     
     var nyse_json = response[0];
     var nasdaq_json = response[1];
+    var screen = response[2];
+    var screen_symbol = "";
 
-    //console.log(ratios);
+    
 
     let data = [];
 
     nyse_json.forEach((val, idx) => { 
-      if ((nyse_json[idx]["price"] < nyse_json[idx]["open"]) && (nyse_json[idx]["price"] >= nyse_json[idx]["previousClose"] ) ) {
-        data.push({symbol: nyse_json[idx]["symbol"], name: nyse_json[idx]["name"], price: nyse_json[idx]["price"], change: nyse_json[idx]["change"], changesPercentage: nyse_json[idx]["changesPercentage"]});
+
+      screen_symbol = screen.filter(x => (x.symbol === nyse_json[idx]["symbol"]));
+
+      if (screen_symbol.length > 0) {
+
+        if ((nyse_json[idx]["price"] < nyse_json[idx]["open"]) && (nyse_json[idx]["price"] >= nyse_json[idx]["previousClose"] )&&(screen_symbol[0].symbol === nyse_json[idx]["symbol"]) ) {
+          data.push({symbol: nyse_json[idx]["symbol"], name: nyse_json[idx]["name"], price: nyse_json[idx]["price"], change: nyse_json[idx]["change"], changesPercentage: nyse_json[idx]["changesPercentage"], beta: screen_symbol[0].beta});
+        }
       }
 
     });
     nasdaq_json.forEach((val, idx) => {
-      if ((nasdaq_json[idx]["price"] < nasdaq_json[idx]["open"]) && (nasdaq_json[idx]["price"] >= nasdaq_json[idx]["previousClose"] ) ) {
-        data.push({symbol: nasdaq_json[idx]["symbol"], name: nasdaq_json[idx]["name"], price: nasdaq_json[idx]["price"], change: nasdaq_json[idx]["change"], changesPercentage: nasdaq_json[idx]["changesPercentage"]});
+
+      screen_symbol = screen.filter(x => (x.symbol === nasdaq_json[idx]["symbol"]));
+
+      if (screen_symbol.length > 0) {
+
+        if ((nasdaq_json[idx]["price"] < nasdaq_json[idx]["open"]) && (nasdaq_json[idx]["price"] >= nasdaq_json[idx]["previousClose"] ) &&(screen_symbol[0].symbol === nasdaq_json[idx]["symbol"])) {
+          data.push({symbol: nasdaq_json[idx]["symbol"], name: nasdaq_json[idx]["name"], price: nasdaq_json[idx]["price"], change: nasdaq_json[idx]["change"], changesPercentage: nasdaq_json[idx]["changesPercentage"], beta: screen_symbol[0].beta});
+        }
       }
+
     });
     //console.log(nyse)
 
@@ -52,6 +67,7 @@ async function fetchData(urlstrings) {
         { title: "price"},
         { title: "change"},
         { title: "changesPercentage"},
+        { title: "beta"},
 
       ];
 
