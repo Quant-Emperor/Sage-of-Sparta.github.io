83b49c4767073f913e2cced71af12bbd10e64de3
Damian Joseph <damianvjoseph@gmail.com>
2021-10-04 07:25:29 +1100

    fix up dashboard

--- js/stockdash.js
+++ js/stockdash.js
@@ -23,8 +23,9 @@ async function fetchData(urlstrings) {
     
     var ratios = response[0];
     var hist_price = response[1].historical;
+    var profile = response[2].profile;
 
-    //console.log(ratios);
+    //console.log(profile["symbol"]);
 
     let pe = [], stockadjcloseprice = [], peg = [];
 
@@ -64,12 +65,65 @@ async function fetchData(urlstrings) {
 
     renderChart(data_series,'pegchartdiv','PEG');
 
+
+    var column_list = [
+            { title: "Ticker", "data": "symbol" },
+            { title: "Price", "data": "price" },
+            { title: "Beta", "data": "beta" },
+            { title: "volAvg", "data": "volAvg" },
+            { title: "Link", "data": "website" 
+
+            ,"render": function(data){
+                       return '<a href="' + website + '">Click here</a>';
+                     }
+        },
+        ];
+
+      
+     let data = [];
+
+      //profile.forEach((val, idx) => { 
+
+        //var dataset = [[profile["symbol"],profile["price"],profile["beta"],profile["volAvg"],profile["website"]],
+        //              [profile["symbol"],profile["price"],profile["beta"],profile["volAvg"],profile["website"]]
+        //];
+
+var dataSet = [
+    [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+    [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
+    [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
+    [ "Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060" ],
+    [ "Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700" ],
+];
+
+
+
+      //  console.log(dataset);
+        //console.log(jsonobj.articles[idx]);
+      //});
+      var data_val = data.map(el => Object.values(el));
+      var table4 = $('#table4').DataTable();
+      table4.destroy();
+
+      var dataTables = $('#table4').DataTable ({
+            data:dataSet,
+            columns : [
+            { title: "Name" },
+            { title: "Position" },
+            { title: "Office" },
+            { title: "Extn." },
+            { title: "Start date" },
+            { title: "Salary" }
+        ]
+        });
+
+
+
+
   } catch (error) {
     console.log("Error", error)
   }
 }
-
-
 
 
 function renderChart(series,jscchartname,title) {
